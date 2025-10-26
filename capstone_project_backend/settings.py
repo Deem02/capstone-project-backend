2be865d1b66c17836d201187363df81cdbbace13
Deem02 <Deem2002q@gmail.com>
2025-10-26 15:33:43 +0300

    Removed venv folder from github repo

--- capstone_project_backend/settings.py
+++ capstone_project_backend/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'capstone_project_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'capstone_project',  # Name of your PostgreSQL database
+        'USER': 'postgres',
+        'PASSWORD': 'De321$',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
