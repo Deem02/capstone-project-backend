924a5b751627b04af3f7a00f8298f62192b45359
Deem02 <Deem2002q@gmail.com>
2025-10-28 18:52:35 +0300

    Create Employee model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
 
 class Department(models.Model):
     name = models.CharField(max_length=200, unique=True)
@@ -8,3 +11,21 @@ class Department(models.Model):
     
     def __str__(self):
         return self.name
+
+ 
+ROLE_CHOICES =[
+    ('USER','User'),
+    ('ADMIN','Admin'),
+]    
+class Employee(models.Model):
+    user= models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='employee_profile'
+    )
+    
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, )
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, blank=True, null=True, related_name='employees')
+    
+    def __str__(self):
+        return self.user.username
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('departments/',DepartmentList.as_view(),name='department_list'),
-    path('departments/<int:department_id>/',DepartmentDetail.as_view(),name='department_list'),
+    path('departments/<int:department_id>/',DepartmentDetail.as_view(),name='department_detail'),
        path('login/', TokenObtainPairView.as_view(), name='login'),
        path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
